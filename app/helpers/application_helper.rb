6135ab4682c365ee97d2ba6b17a6e41d6745a70a
Michael Lai <themichaellai@gmail.com>
2013-04-07 02:51:38 -0400

    Player improvements, styling, sample info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def seconds_to_display(seconds)
+    puts "CONVERTING #{seconds}"
+    mins = seconds / 60
+    if mins > 0
+      secs = seconds % 60
+    else
+      secs = seconds
+    end
+    secs = "0#{secs}" if secs < 10
+    "#{mins}:#{secs}"
+  end
 end
